4e45e4c0b888e7a3392f714caa9016d7e37220a2
Peds Yorpo <pedroyorpo17@gmail.com>
2025-03-01 14:31:26 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/uploads/{path}', function ($path) {
+    return response()->file(storage_path('app/uploads/' . $path));
+})->where('path', '.*');
+
 // Route::resource('profiles',ProfileController::class)->names('profiles');
 
 Route::resource('profiles',ProfilepictureController::class)->names('profiles');
